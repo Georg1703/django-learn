a2e7a7584d500564232ee71aedd27aae71d9a0e3
Tudor1703 <tudormirca90@gmail.com>
2024-10-09 22:12:19 +0300

    Reverse Prefix of Word

---
+++ Pr.2000.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        idx = word.find(ch)
+        
+        if idx != -1:
+            return word[:idx + 1][::-1] + word[idx + 1:]
+        else:
+            return word
+'''
+I discovered the function find()
+'''
